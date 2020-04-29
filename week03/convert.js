6edff51992ed340e8b26244c1951634155152e93
woowyl <woowyl@163.com>
2020-04-29 18:06:05 +0800

    homework

--- week03/convert.js
+++ week03/convert.js
@@ -1,9 +1,27 @@
-function stringConvertNumber() {
 
+/**
+ * 
+ * @param {*} string 
+ * @param {*} hex 
+ * case: 
+ *   decimal: 10, 10.123, 0.3, .345
+ *   octal: 0o101 
+ *   hex: 0x101
+ */
+function ConvertStringToNumber(string, hex) {
 
+    var chars = string.split('');
+    
+    var number = 0;
+
+    chars.forEach((ele, index) => {
+        number += chars[index].codePointAt(0) - '0'.codePointAt(0)
+    });
+
+    return number;
 }
+ConvertStringToNumber("10.123", 10)
 
-
-function numberConvertString() {
+function ConvertnumberToString() {
     
 }
